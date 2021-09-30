3bc726cf3f64194fdf6542ef6236c886b28db481
tom <32755@ma-web.nl>
2021-09-30 15:14:26 +0200

    Daily Choices

---
+++ Daily Choices.py
@@ -0,0 +1,10 @@
+while True:
+    cho = input("ga je naar buiten? ")
+    if cho == "ja":
+        print("je gaat naar buiten")
+        break
+    elif cho == "nee":
+        print("je bleef binnen")
+        break
+    else:
+        print("ja of nee")
