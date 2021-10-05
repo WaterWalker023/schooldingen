b6466780aa72ed15cf7d8ae020c48b2565465394
WaterWalker023 <32755@ma-web.nl>
2021-10-05 09:33:37 +0200

    1+1 en G = 1

---
+++ 1+1.py
@@ -0,0 +1,7 @@
+V = 1
+print(V)
+while True:
+    V = input("1+1=")
+    if V == "2":
+        print(":")
+        break
---
+++ g = 1.py
@@ -0,0 +1,8 @@
+g = 1
+print(1)
+
+while True:
+    g += 1
+    print(g)
+    if g == 1000:
+        break
